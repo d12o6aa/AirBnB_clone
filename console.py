457b98c59faac22e2b01f265ad4dcf5d2d2f6a2a
d12o6aa <doaakarem798@gmail.com>
2023-10-13 15:04:34 +0300

    method create

--- console.py
+++ console.py
@@ -7,6 +7,13 @@ import sys
 from datetime import datetime
 import models
 from models.base_model import BaseModel
+#from models.user import User
+#from models.state import State
+#from models.city import City
+#from models.amenity import Amenity
+#from models.place import Place
+#from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -14,6 +21,8 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = '(hbnb)'
+    classes = ['BaseModel', 'User', 'State', 'City',
+               'Amenity', 'Place', 'Review']
 
     def do_EOF(self, line):
         """
@@ -32,6 +41,20 @@ class HBNBCommand(cmd.Cmd):
         empty lines
         """
         pass
+
+    def do_create(self, line):
+        """
+         Creates a new instance of BaseModel
+        """
+        command = self.parseline(line)[0]
+        if command is None:
+            print("** class name missing **")
+        elif command not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            new_obj = eval(command)
+            new_obj.save()
+            print(new_obj.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
