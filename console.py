4c42b632d83ad1f31639f9168510ebdf10814951
d12o6aa <doaakarem798@gmail.com>
2023-10-14 11:26:03 +0300

    done

--- console.py
+++ console.py
@@ -165,11 +165,11 @@ class HBNBCommand(cmd.Cmd):
 if __name__ == '__main__':
     shell = HBNBCommand()
 
-<<<<<<< HEAD
+# <<<<<<< HEAD
 
-=======
+# =======
 
->>>>>>> e3fa2e2357489c2fccf2f78c932f93a86bc26afc
+# >>>>>>> e3fa2e2357489c2fccf2f78c932f93a86bc26afc
 
     if not sys.stdin.isatty():
         for line in sys.stdin:
